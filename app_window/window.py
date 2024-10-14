411bea62502402862cffdce7e87ead4c65947c7c
Bor0dah <sahsa88395@gmail.com>
2024-10-14 21:53:34 +0300

    Женя приказал
    Женя сказал сноси нахер строки с 16-18 и строку 11, я следую его указаниям, но есть опасения что если не оправдаю его то меня снесут из друзей и контактов ;)

--- app_window/window.py
+++ app_window/window.py
@@ -8,14 +8,12 @@ def get_text():
 window = Tk()
 window.geometry('700x700')
 
-canvas = Canvas(window, width=500, height=700)
+
 text_box = Text(window, font=('Arial', 15), wrap='word', height=5)
 button_send = Button(window, text='Send', command=get_text)
 label_sended = Label(canvas, text='', height=15, font=('Arial', 15))
 
-scrollbar = Scrollbar(canvas, orient=VERTICAL, command=canvas.yview)
-scrollbar.place(relx=1, rely=0, relheight=1, anchor=NE)
-canvas.config(yscrollcommand=scrollbar.set, scrollregion=(0, 0, 0, 0))
+
 
 label_sended.pack()
 text_box.pack()
